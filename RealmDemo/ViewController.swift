c8220d7ed6f128d680ae057e47efbd89e1021069
David Lin <yoxisem544@gmail.com>
2016-11-21 14:10:58 +0800

    Initial Commit

---
+++ RealmDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RealmDemo
+//
+//  Created by David on 2016/11/21.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
